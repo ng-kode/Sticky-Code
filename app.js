83393f674aeffcb0339ebadd91ef3432089d5e51
Richard Ng <richard.ng@corp.agscreative.com>
2018-03-21 16:19:53 +0800

    Express serves React

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public/build')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
