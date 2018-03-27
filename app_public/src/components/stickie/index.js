b00ba1f65a85098322eb899cd73effacc38b9d45
richard.ng <richard.chihong.ng@gmail.com>
2018-03-28 00:22:31 +0800

    ace editor

--- app_public/src/components/stickie/index.js
+++ app_public/src/components/stickie/index.js
@@ -4,16 +4,45 @@ import { connect } from 'react-redux'
 import {  } from '../../actions'
 
 class Stickie extends Component {
+
+    createDivAce = () => {
+        const { $, ace } = this.props.window        
+        this._stickieEditor = ace.edit(this._stickie)
+        this._stickieEditor.setValue(`
+this._stickieEditor.setOptions({
+    readOnly: true,
+    highlightActiveLine: false,
+    showGutter: false  
+})
+this._stickieEditor.clearSelection(1);
+this._stickieEditor.setTheme("ace/theme/solarized_light");
+this._stickieEditor.session.setMode('ace/mode/text');
+this._stickieEditor.container.style.pointerEvents="none"
+        `);
+        this._stickieEditor.setOptions({
+            readOnly: true,
+            highlightActiveLine: false,
+            showGutter: false,
+            wrapBehavioursEnabled: true
+        })
+        this._stickieEditor.clearSelection(1);
+        this._stickieEditor.setTheme("ace/theme/solarized_light");
+        this._stickieEditor.session.setMode(`ace/mode/text`);
+        this._stickieEditor.container.style.pointerEvents="none"
+
+        $(this._stickie).append('<div class="border-ph-right"></div>')
+        $(this._stickie).append('<div class="border-ph-bottom"></div>')
+    }
+    
+    
+    componentDidMount() {
+        this.createDivAce()
+    }
+    
+
     render() {
         return (
-            <div className="card stickie">
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
-                </div>
+            <div className="stickie" ref={component => this._stickie = component}>                
             </div>
         )
     }
@@ -21,7 +50,7 @@ class Stickie extends Component {
 
 
 const mapStateToProps = (state, ownProps) => ({
-    
+    ...state, ...ownProps
 })
 
 const mapDispatchToProps = {
